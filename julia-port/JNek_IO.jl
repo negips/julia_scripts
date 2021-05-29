857c94adbbb04487c110469a8ebac3d2abfcf7cd
Prabal Negi <negi@kth.se>
2021-05-29 17:36:35 +0200

    updating

--- julia-port/JNek_IO.jl
+++ julia-port/JNek_IO.jl
@@ -4,31 +4,55 @@
 
       module JNek_IO
 
-#      include("nek_mpi.jl")
+      using MPI
 
-      export read_re2_hdr, read_re2, read_ma2
+      export read_re2_hdr,
+             read_re2, 
+             read_ma2
 
-      function read_re2_hdr(fid::IOStream, rank, nid0)
+      function __init__()
 
-#        comm = MPI.COMM_WORLD
+        if MPI.Initialized() == false
 
-        if rank == nid0
-          println("Reading Header on rank $(rank)")
+          MPI.Init()
+
+        end    
+          
+        comm = MPI.COMM_WORLD
+        rank = MPI.Comm_rank(comm)
+
+        if rank == 0
+          println("Initialied MPI in Module JNek_IO")
+        end  
+
+        return nothing
+      end 
+
+#----------------------------------------------------------------------
+
+      function read_re2_hdr(fid::IOStream, rank)
+
+#        hdr = repeat(" ", 26)
+#        version = repeat(" ", 5)
+#        nelgt = 0
+#        nelgv = 0
+#        ldimr = 0
+
+        println("Reading Header on rank $(rank)")
         
-          nbytes  = 26
-          hdrutf  = read(fid,nbytes)
-          hdrC    = Char.(hdrutf)
-          version = String(hdrC[1:5])
-          nelgtS  = String(hdrC[6:14])
-          ldimrS  = String(hdrC[15:17])
-          nelgvS  = String(hdrC[18:26])
+        nbytes  = 26
+        hdrutf  = read(fid,nbytes)
+        hdrC    = Char.(hdrutf)
+        version = String(hdrC[1:5])
+        nelgtS  = String(hdrC[6:14])
+        ldimrS  = String(hdrC[15:17])
+        nelgvS  = String(hdrC[18:26])
 
-          nelgt   = parse(Int, nelgtS)
-          ldimr   = parse(Int, ldimrS)
-          nelgv   = parse(Int, nelgvS)
+        nelgt   = parse(Int, nelgtS)
+        ldimr   = parse(Int, ldimrS)
+        nelgv   = parse(Int, nelgvS)
 
-          hdr     = String(hdrC) 
-        end
+        hdr     = String(hdrC) 
 
 
         return hdr,version,nelgt,ldimr,nelgv
@@ -36,8 +60,13 @@
 
 #---------------------------------------------------------------------- 
 
-      function read_re2(f::String, MPI::Module, nid0::Int64)
+      function read_re2(f::String, nid0::Int64)
 
+        hdr = repeat(" ", 26)
+        version = repeat(" ", 5)
+        nelgt = 0
+        nelgv = 0
+        ldimr = 0
 
         comm = MPI.COMM_WORLD
 
@@ -46,17 +75,21 @@
         end
         
         MPI.Barrier(comm)
-
-        fid = open(f, "r")
-
         rank = MPI.Comm_rank(comm)
 
-        hdr,version,nelgt,ldimr,nelgv = read_re2_hdr(fid,rank, nid0);
+        if (rank == nid0)
+          fid = open(f, "r")
+        end  
 
-        buf = MPI.Buffer(hdr,length(hdr),MPI.CHAR)
+        if rank == nid0
+          hdr,version,nelgt,ldimr,nelgv = read_re2_hdr(fid,rank)
+        end  
+
+
+        buf = MPI.Buffer(hdr,26,MPI.CHAR)
         MPI.Bcast!(buf,     nid0,comm)
 
-        buf = MPI.Buffer(version,length(version),MPI.CHAR)
+        buf = MPI.Buffer(version,5,MPI.CHAR)
         MPI.Bcast!(buf,     nid0,comm)
 
         nelgt = MPI.bcast(nelgt,     nid0,comm)
@@ -65,44 +98,35 @@
 
 #       Read the data here        
 
-        close(fid)
+        if rank == nid0
+          close(fid)
+        end  
 
         return hdr,version,nelgt,ldimr,nelgv
       end     # read_re2
 
 #---------------------------------------------------------------------- 
 
-      function read_ma2(f::String, MPI::Module, nid0::Int64)
+      function read_ma2(f::String, nid0::Int64)
 
 
         comm = MPI.COMM_WORLD
 
         if MPI.Comm_rank(comm) == nid0
-          println("Reading $(f) on rank $(MPI.Comm_rank(comm))\n")
+          println("Reading $(f) on rank $(MPI.Comm_rank(comm))")
         end
         
         MPI.Barrier(comm)
 
-        fid = open(f, "r")
-
         rank = MPI.Comm_rank(comm)
 
-#        MPI.Bcast!(hdr,nid0,MPI.CHAR)
-#        MPI.Bcast!(version,   nid0,MPI.CHAR)
-#        MPI.Bcast!(nelgt,     nid0,MPI.INT64_T)
-#        MPI.Bcast!(ldimr,     nid0,MPI.INT64_T)
-#        MPI.Bcast!(ldimr,     nid0,MPI.INT64_T)
-
-#       testing        
-        if rank == 1
-          println(hdr)
-        end  
+        if rank == nid0
+          fid = open(f, "r")
+        end
 
         close(fid)
 
-#        MPI.Finalize()
-
-        return hdr,version,nelgt,ldimr,nelgv
+        return nothing
       end     # read_ma2
 
 #---------------------------------------------------------------------- 
