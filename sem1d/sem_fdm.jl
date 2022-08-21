060920a1b8e88e756ca2ed74b33cb35cfe4ec807
Prabal Negi <negi@kth.se>
2022-08-21 23:11:00 +0200

    built a modified nek operator

--- sem1d/sem_fdm.jl
+++ sem1d/sem_fdm.jl
@@ -1,6 +1,7 @@
 println("Testing Fast Diagonalization Method")
 
 using PolynomialBases
+using FastGaussQuadrature
 using LinearAlgebra
 using PyPlot
 
@@ -20,8 +21,10 @@ VT  = Complex{prec}
 one = prec(1.0)
 zro = prec(0.0)
 
+close("all")
+
 # define nodal bases
-N           = 7 ;                               # polynomial degree
+N           = 13 ;                               # polynomial degree
 lx1         = N+1;                              # No of points
 Basis       = LobattoLegendre(N, prec)          # Polynomial Basis
 
@@ -144,11 +147,11 @@ end
 # This is what Nek does:
 dgl = zeros(prec,lx2,lx1)
 for i in 1:lx2
-  z = zgm2[i]
+  zi = zgm2[i]
   for j in 1:lx1
     v = zeros(prec,lx1)
     v[j] = 1.0
-    dgl[i,j] = derivative_at(z,v,zgm1,Basis.baryweights)
+    dgl[i,j] = derivative_at(zi,v,zgm1,Basis.baryweights)
   end
 end  
 
@@ -176,9 +179,6 @@ for j=1:n-1
 end
 lapnek[1,1]       = 1.0
 lapnek[lx1,lx1]   = 1.0
-
-#Interpolation operator to Mesh2
-intpm12 = zeros(VT,lx1,lx1);
 
 jgl  = interpolation_matrix(zgm2,Basis.nodes,Basis.baryweights);
 bjgl = bm2*jgl  
@@ -218,35 +218,137 @@ massnek[lx1,lx1]   = 1.0
 #   enddo
 #enddo
 
+#---------------------------------------------------------------------- 
 
+# This is what I think Nek Should do:
+z     = Geomx.xm1[:,1];
+z12   = copy(z);
+z12[2:lx1-1] = Geom2x.xm1[:,1];
+
+d12   = dxm1*z12
+dgl2 = zeros(prec,lx1,lx1)
+
+#for i in 1:lx1
+#  zi = z12[i]
+#  for j in 1:lx1
+#    v = zeros(prec,lx1)
+#    v[j] = 1.0
+#    dgl2[i,j] = derivative_at(zi,v,zgm1,Basis.baryweights)
+#  end
+#end  
+
+drdx_new = 1.0./d12
+dgl12 = Diagonal(drdx_new)*dxm1
+
+bm12  = Diagonal(wzm1.*d12)
+
+lapNew = zeros(prec,lx1,lx1)
+b0 = 1
+b1 = (lx1)
+
+for j=1:lx1
+  for i=1:lx1
+    s = 0.0
+    for k=b0:b1
+      bb = (wzm1[k]*d12[k])
+      s1 = dgl12[k,i]*bb*dgl12[k,j]
+      s = s + s1 
+    end  
+    lapNew[i,j] = s
+  end
+end
+lapNew[1,:]       = zeros(prec,1,lx1)
+lapNew[1,1]       = 1.0
+lapNew[lx1,:]     = zeros(prec,1,lx1)
+lapNew[lx1,lx1]   = 1.0
+
+#jgl2  = interpolation_matrix(z12,Basis.nodes,Basis.baryweights);
+#bjgl2 = bm12*jgl2
+jgl12  = Matrix{Float64}(I,lx1,lx1)
+
+massNew     = zeros(prec,lx1,lx1)
+
+b0 = 1
+b1 = (lx1)
+for j=1:lx1
+  for i=1:lx1
+    s = 0.0
+    for k=b0:b1
+      bb = (wzm1[k]*d12[k])
+      s1 =  jgl12[k,i]*bb*jgl12[k,j]
+#      println("$i $j $k $bb $s1")
+      s = s + s1 
+    end  
+    massNew[i,j] = s
+  end
+end
+massNew[1,:]         = zeros(prec,1,lx1)
+massNew[1,1]         = 1.0
+massNew[lx1,:]       = zeros(prec,1,lx1)
+massNew[lx1,lx1]     = 1.0
+
+
+#---------------------------------------------------------------------- 
 # Solving the linear System
-α     = 2.0
 x     = XM1[:,1];
 x12   = copy(x);
 x12[2:lx1-1] = Geom2x.xm1;
-f     = -Geomx.bm1[:,1].*((α*π)^2).*sin.(α*π*x);
+
+α     = 3.0
+f     = -((α*π)^2).*sin.(α*π*x);
+fn    = -((α*π)^2).*sin.(α*π*x12);
+f12   = -((α*π)^2).*sin.(α*π*x12);
 A     = LAPx[:,:,1];
 B     = BMx[:,:,1];
 # Set Boundary conditions:
-d      = 1.0
+d      = 0.0
 A[1,:] = zeros(prec,1,lx1)
 A[1,1] = 1.0
 B[1,:] = zeros(prec,1,lx1)
 B[1,1] = 1.0
 f[1]   = d
+fn[1]  = d
+f12[1] = d
 
 A[lx1,:]    = zeros(prec,1,lx1)
 A[lx1,lx1]  = 1.0
 B[lx1,:]    = zeros(prec,1,lx1)
 B[lx1,lx1]  = 1.0
 f[lx1]      = d
+fn[lx1]     = d
+f12[lx1]    = d
 
-u           = inv(A)*f
-unek        = inv(lapnek)*f
+u           = inv(A)*(Geomx.bm1[:,1].*f)
+unek        = inv(lapnek)*(Geomx.bm1[:,1].*fn)
+unew        = inv(lapNew)*bm12*f12
 
-plot(x,u)
-plot(x12, unek)
-plot(x,d .- sin.(α*π*x),linestyle="--",linewidth=2)
+# Build an interpolation Matrix on to a fine grid
+N3 = 150
+xf = Vector(range(-1.0,1.0,length=N3))
+jf = interpolation_matrix(xf,Basis.nodes,Basis.baryweights);
+
+bwf = PolynomialBases.barycentric_weights(x12)
+jf12 = interpolation_matrix(xf,x12,bwf);
+
+
+
+soln = d .- sin.(α*π*xf)
+
+plot(xf,jf*u,label="Matrix")
+plot(xf,jf12*unek,label="Nek")
+plot(xf,jf12*unew,label="Modified Nek")
+plot(xf,soln,linestyle="--",linewidth=2,label="Analytical")
+legend(fontsize=8)
+
+# plot(x,u)
+# plot(x12,unek)
+# plot(x,soln,linestyle="--",linewidth=2)
+
+FNek = eigen(lapnek,massnek);
+FNew = eigen(lapNew,massNew);
+FMe  = eigen(A,B);
+
+compare = [FNek.values FNew.values FMe.values];
 
 println("Done")
 
