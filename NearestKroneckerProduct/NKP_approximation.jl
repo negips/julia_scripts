707b11a8a452b40165cd4d44cb456c5419310b24
Prabal Negi <negi.87@gmail.com>
2025-06-05 02:06:34 +0900

    UPDATING - SPECTRE

--- NearestKroneckerProduct/NKP_approximation.jl
+++ NearestKroneckerProduct/NKP_approximation.jl
@@ -34,44 +34,54 @@ end
 #----------------------------------------------------------------------  
 
 
-
-Nx    = 11
+Nx    = 15
 lx1   = Nx+1
 Bx    = LobattoLegendre(Nx)
 Vx    = ones(Float64,lx1)
+Nxpf  = 400
+XfRef = LinRange(-1.0,1.0,Nxpf)
+IntpX = interpolation_matrix(XfRef,Bx.nodes,Bx.baryweights)
 
-Ny    = 11
+Ny    = 15
 ly1   = Ny+1
-B     = LobattoLegendre(Ny)
+By    = LobattoLegendre(Ny)
 Vy    = ones(Float64,ly1)
+Nypf  = 400
+YfRef = LinRange(-1.0,1.0,Nypf)
+IntpY = interpolation_matrix(YfRef,By.nodes,By.baryweights)
 
 xs    = 0.0
 xe    = 1.0
 ys    = 1.0
 ye    = 2.0
 
+
+
 # X-direction
 X     = zeros(Float64,lx1)
 for i in 1:lx1
-  X[i] = map_from_canonical(B.nodes[i],xs,xe,B)
+  X[i] = map_from_canonical(Bx.nodes[i],xs,xe,B)
 end
 X2D   = kron(Vy',X)
+Xf2D  = IntpX*X2D*(IntpY');
+
 
 # Y-direction
 Y     = zeros(Float64,ly1)
 for i in 1:ly1
-  Y[i] = map_from_canonical(B.nodes[i],ys,ye,B)
+  Y[i] = map_from_canonical(By.nodes[i],ys,ye,B)
 end
 Y2D   = kron(Y',Vx)
+Yf2D  = IntpX*Y2D*(IntpY');
 
-rng   = MersenneTwister(1234)
 
+#rng   = MersenneTwister(1234)
 Fld   = zeros(Float64,lx1,ly1)
 for ci in CartesianIndices(Fld)
   x = X2D[ci]
   y = Y2D[ci]
   # Fld[ci] = 0.0*rand(rng) + cos(π*x) + sin(π*y) + tan(x+y)
-  Fld[ci] = cos(π*x)*sin(π*y) + 0.1*rand(rng)
+  Fld[ci] = cos(2*π*x)*sin(2*π*y) + 0.5*rand(rng)
 end
 
 Fv = Fld[:]
@@ -98,22 +108,25 @@ Fy = reshape(fy,ly1,ly1)
 
 Fld_rec = kron(diag(Fy)',diag(Fx))        # Reconstructed field
 
-Fmin  = minimum(Fld[:])
-Fmax  = maximum(Fld[:])
-
 close("all")
-cm2    = get_cmap("Spectral");
-#h1,axs = figure(num=1,figsize=[16.0,6.0])
+
+Fldf   = IntpX*Fld*(IntpY')
+Fldf_r = IntpX*Fld_rec*(IntpY')
+Fmin   = minimum(Fldf[:])
+Fmax   = maximum(Fldf[:])
+
+
+cm2    = get_cmap("RdBu_r");
 h1,axs = subplots(1,3,sharey=true,figsize=[16.0,8.0],layout="constrained" )
-pcm1   = axs[1].pcolormesh(X2D,Y2D,Fld,vmin=Fmin,vmax=Fmax)
+pcm1   = axs[1].pcolormesh(Xf2D,Yf2D,Fldf,vmin=Fmin,vmax=Fmax)
 pcm1.set_cmap(cm2)
 cb1    = colorbar(pcm1,location="left")
 
-pcm2  = axs[2].pcolormesh(X2D,Y2D,Fld_rec,vmin=Fmin,vmax=Fmax)
+pcm2   = axs[2].pcolormesh(Xf2D,Yf2D,Fldf_r,vmin=Fmin,vmax=Fmax)
 pcm2.set_cmap(cm2)
 #cb2    = colorbar(pcm2,location="top")
 
-pcm3  = axs[3].pcolormesh(X2D,Y2D,(Fld .- Fld_rec),vmin=Fmin,vmax=Fmax)
+pcm3   = axs[3].pcolormesh(Xf2D,Yf2D,(Fldf .- Fldf_r),vmin=Fmin,vmax=Fmax)
 pcm3.set_cmap(cm2)
 
 #ax3   = h3.gca()
