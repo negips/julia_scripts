c980c4d623437f8af58b64643f3473e8d8ea649c
Prabal Negi <negi.87@gmail.com>
2025-10-07 18:58:27 +0900

    UPDATING - SPECTRE

--- center_manifold/testing.jl
+++ center_manifold/testing.jl
@@ -58,6 +58,7 @@ ALg   = QT*AL*Q
 Bg    = QT*B
 Bgi   = 1.0./Bg
 OPg   = Bgi.*Lg
+AOPg  = Bgi.*ALg
 
 # Stepper-Arnoldi
 #-------------------------------------------------- 
@@ -82,24 +83,39 @@ tol               = 1.0e-10
 ArnInp            = StepperArnoldi.ArnoldiInput(ifarnoldi,ifverbose,vlen,nev,ekryl,lkryl,ngs,bsize,outer_iterations,tol)
 
 Ω     = SEM1D.GLAnalyticalSpectra(δ)
+if (StpInp.ifadjoint)
+  Ω   = conj.(Ω)
+end  
+
+close("all")
 h1    = figure(num=1,figsize=[8.,6.]);
 ax1   = gca()
 pl1   = ax1.plot(imag.(Ω),real.(Ω),linestyle="none",marker="o",markersize=8)
 ax1.set_ylim([-2.75,0.5])
-ax1.set_xlim([0.0,1.80])
+if (StpInp.ifadjoint)
+  ax1.set_xlim([-1.80,0.0])
+else  
+  ax1.set_xlim([0.0,1.80])
+end  
 
 #ev2   = eigvals(Matrix(OPg))
 #pl2   = ax1.plot(imag.(ev2),real.(ev2),linestyle="none",marker="x",markersize=4)
 
 # Calculate Spectra
-ArnOut            = StepperArnoldi.StepArn(OPg,Bg,StpInp,ArnInp,Inp.lbc,Inp.rbc)
+if StpInp.ifadjoint
+  ArnOut      = StepperArnoldi.StepArn(AOPg,Bg,StpInp,ArnInp,Inp.lbc,Inp.rbc)
+else  
+  ArnOut      = StepperArnoldi.StepArn(OPg,Bg,StpInp,ArnInp,Inp.lbc,Inp.rbc)
+end  
 
 pl3   = ax1.plot(imag.(ArnOut.evals),real.(ArnOut.evals),linestyle="none",marker="s",markersize=4)
 
-# xg    = QT*(vimult.*GeoM.xm1[:])
-# h2    = figure(num=2,figsize=[8.,6.])
-# ax2   = gca()
-# pl3   = plot(xg,real.(w))
+xg    = QT*(vimult.*GeoM.xm1[:])
+h2    = figure(num=2,figsize=[8.,6.])
+ax2   = gca()
+for i in 1:ArnInp.nev
+  ax2.plot(xg,real.(ArnOut.evecs[:,i]))
+end  
 
 println("Done.")
 
