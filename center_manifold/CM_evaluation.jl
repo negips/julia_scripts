6f6bf7a8041d7a1d5e9a5f1936d79369f20116e3
Prabal Negi <negi.87@gmail.com>
2025-10-13 14:55:47 +0900

    UPDATING - SPECTRE

---
+++ center_manifold/CM_evaluation.jl
@@ -0,0 +1,17 @@
+# Center Manifold Evaluation
+println("Center-Manifold Evaluation on the Ginzburg Landau system.")
+
+#include("GL_Standard_Tangent_Space.jl")
+
+#include("GL_Extended_Tangent_Space.jl")
+
+include("GL_Asymptotic_CM_NormalForm.jl")
+
+println("Done.")
+
+
+
+
+
+
+
