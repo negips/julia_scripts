cf4436030890483beb096686ada5071fa899bee3
Prabal <negi.87@gmail.com>
2025-12-08 22:11:31 +0900

    UPDATE - WORKSTATION

--- center_manifold/GL_NonLinear_Parametric.jl
+++ center_manifold/GL_NonLinear_Parametric.jl
@@ -47,7 +47,7 @@ close("all")
 ifplot      = true
 histplot    = true
 verbose     = true
-nsteps      = 50000000
+nsteps      = 6000000
 ifsave      = false
 plotstep    = 100000
 verbosestep = 100000
@@ -56,7 +56,7 @@ nhist       = Int(nsteps/histstep)
 hist_x      = 10.0                        # Location of history point
 hist_i      = argmin(abs.(xg .- hist_x))  # Index of history point
 nfreq       = 1                           # No. of external frequencies
-dt          = 0.00002
+dt          = 0.00005
 Tend        = dt*nsteps
 
 #θA          = [0.1; 0.25; 0.5; 0.75; 1.0]
